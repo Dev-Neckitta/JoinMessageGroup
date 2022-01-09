4753c4d495c477b70c5c8a3a6e567ff741238e07
MulqiGaming64 <mulqi311@gmail.com>
2022-01-09 11:54:48 +0700

    Update and rename src/msggroup/main.php to src/Neckitta/JoinMessageGroup/JoinMessageGroup.php

---
+++ src/Neckitta/JoinMessageGroup/JoinMessageGroup.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Neckitta\JoinMessageGroup;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\{PlayerJoinEvent, PlayerQuitEvent};
+use pocketmine\{Player, Server};
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class JoinMessageGroup extends PluginBase implements Listener{
+
+	public function onEnable(): void{
+		$this->saveResource("config.yml");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	/** @param PlayerJoinEvent $ev */
+	public function onJoin(PlayerJoinEvent $ev){
+		$player = $ev->getPlayer();
+		$name = $player->getName();
+		foreach($this->getConfig()->getAll()["list"] as $rank => $data){
+			if($player->hasPermission($data["permission"])){
+				$msg = str_replace("{name}", $name, $data["join"]);
+       		 $ev->setJoinMessage($msg);
+       	 }
+		}
+	}
+	
+	/** @param PlayerQuitEvent $ev */
+	public function onQuit(PlayerQuitEvent $ev){
+		$name = $player->getName();
+		foreach($this->getConfig()->getAll()["list"] as $rank => $data){
+			if($player->hasPermission($data["permission"])){
+				$msg = str_replace("{name}", $name, $data["quit"]);
+       		 $ev->setQuitMessage($msg);
+       	 }
+		}
+	}
+}
